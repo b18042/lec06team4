9e3ffa5eee23888cb3d01d8d3b5f1fe7bfcc7446
b18042 <e1b18042@oit.ac.jp>
2020-11-10 15:59:08 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/chat4/lec06team4/Lec06team4Application.java
@@ -0,0 +1,13 @@
+package oit.is.chat4.lec06team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec06team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec06team4Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/chat4/lec06team4/Lec06team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.chat4.lec06team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec06team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
