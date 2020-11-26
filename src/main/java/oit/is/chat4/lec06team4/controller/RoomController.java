0d95acb90111db9328da26d244b2e02711ac309b
e1b18030 <e1b18030@oit.ac.jp>
2020-11-27 03:02:06 +0900

    各ルームにログインしているアカウントの表示

--- src/main/java/oit/is/chat4/lec06team4/controller/RoomController.java
+++ src/main/java/oit/is/chat4/lec06team4/controller/RoomController.java
@@ -11,25 +11,36 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class RoomController {
+
   @GetMapping("/Room1")
-  public String Room1Entry() {
+  public String Room1Entry(ModelMap model, Principal prin) {
 
     //model.addAttribute("User1", User1);
     // model.addAttribute("User2", User2);
     //model.addAttribute("Match1", Match1);
 
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    //return "sample31.html";
+
     return "Room1.html";
   }
 
   @GetMapping("/Room2")
-  public String Room2Entry() {
+  public String Room2Entry(ModelMap model, Principal prin) {
 
     // model.addAttribute("User1", User1);
     // model.addAttribute("User2", User2);
     // model.addAttribute("Match1", Match1);
+
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    // return "sample31.html";
 
     return "Room2.html";
   }
