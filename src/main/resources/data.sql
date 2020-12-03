4df2c58a3ec0689a5154f8fca2cd82d04f64dabc
e1b18073 <e1b18073@oit.ac.jp>
2020-12-03 18:44:15 +0900

    各テーブルにレコードを登録

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Room1 (user,chatlog) VALUES ('user1','こんにちは');
+INSERT INTO Room1 (user,chatlog) VALUES ('user2','よろしく');
+INSERT INTO Room2 (user,chatlog) VALUES ('user1','こんにちは');
+INSERT INTO Room2 (user,chatlog) VALUES ('user2','よろしく');
